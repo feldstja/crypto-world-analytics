f436fdf66893cd62a2b1e6eb9bd46225db28cc74
Jacob <jacob.a.feldstein@vanderbilt.edu>
2018-07-31 15:52:07 -0700

    paul

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+const axios = require('axios')
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(bodyParser.json())
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+// DO NOT REMOVE THIS LINE :)
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/currency', function(req, res){
+  axios("https://api.hitbtc.com/api/2/public/ticker", {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'application/json'
+    },
+    mode: 'no-cors'
+  })
+  .then((resp)=>{
+    var newArr = resp.data.map(({symbol, timestamp, volume})=> (
+      {symbol, timestamp, volume}
+    ))
+    console.log(newArr)
+      // this.setState({
+      //   arr: resp
+      // })
+      res.json(newArr)
+  }).catch(function(err) {
+    console.log('ERROR:', err)
+  });
+})
+
+app.listen(process.env.PORT || 1337);
